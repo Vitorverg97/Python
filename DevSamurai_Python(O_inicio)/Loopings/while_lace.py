25d43a48eb9d3b3dea6df3f83da59355436edcfc
Vitorverg97 <vitor.vsouza05@gmail.com>
2025-02-02 16:44:28 -0300

    Second exercise - while loops - PTBR

--- DevSamurai_Python(O_inicio)/Loopings/while_lace.py
+++ DevSamurai_Python(O_inicio)/Loopings/while_lace.py
@@ -6,21 +6,21 @@
 # O laço while repete até que uma condição seja atendida, exemplo:
 ## Contador de números (Number Counter)
 
-#numero = 0
-#while numero <= 10:
-  #  print(numero)
- #   numero += 1 # Forma abreviada da expressão: numero = numero + 1
-#print ('Acabou o loop while.')
+numero = 0
+while numero <= 10:
+    print(numero)
+    numero += 1 # Forma abreviada da expressão: numero = numero + 1
+print ('Acabou o loop while.')
 
 ## Contador de números com input (Number Counter with input)
 
-#numero_max = int(input('Insira o número que deseja contar: '))
-#NUMERO_INIT = 0
+numero_max = int(input('Insira o número que deseja contar: '))
+NUMERO_INIT = 0
 
-#while NUMERO_INIT <= numero_max:
-  #  print(NUMERO_INIT)
- #   NUMERO_INIT += 1
-#print('Fim da contagem.')
+while NUMERO_INIT <= numero_max:
+    print(NUMERO_INIT)
+    NUMERO_INIT += 1
+print('Fim da contagem.')
 
 #___EXERCISE___#
 
@@ -32,3 +32,23 @@ while numero_fat >=2:
     fatorial = fatorial * (numero_fat - 1)
     numero_fat -=1
 print(f"O fatorial é {fatorial}.")
+
+## Faça um programa que simule um cofre, ele terá uma senha pré-definida. A cada tentativa errada o programa deverá mostrar a mensagem: Senha incorreta. Tente novamente. Se ele acertar a senha o programa deverá mostrar a mensagem: Senha correta. Acesso concedido.
+
+senha = '777'
+tentativas = 3
+
+while tentativas > 0:
+    cofre_pessoal = input("Digite a senha que contém 3 números: ")
+    
+    if cofre_pessoal == senha:
+        print("Senha correta. Acesso concedido.")
+        break
+    
+    tentativas -= 1
+    
+    if tentativas == 0: 
+        print("Limite de tentativas excedido. Tente novamente mais tarde.")
+    
+    else :
+        print(f"Senha incorreta. Tente novamente.({tentativas} tentativas restantes)")
